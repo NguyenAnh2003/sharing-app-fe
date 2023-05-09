120abc56d6fc2f7ca9d7565f31376f0d481f14be
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-05-09 19:31:21 +0700

    #code

--- src/libs/utils/axiosClient.js
+++ src/libs/utils/axiosClient.js
@@ -1,1 +1,12 @@
 import axios from 'axios';
+
+// take userInfo from cookies ?
+
+const axiosClient = axios.create({
+  baseURL: '',
+  headers: {},
+  // content-type: application/json
+  
+});
+
+export default axiosClient;
