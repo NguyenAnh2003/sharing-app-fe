bf2fb3bf66e92ff529f11ba4b773a534ba99e083
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-05-20 22:00:09 +0700

    #code

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
+/**
+ * 
+ */
+
 const PostCard = React.memo((props) => {
+  console.log("Re-render from postCard?");
   return (
     <div>
       
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
+/**
+ * UserCard used for searching page the properties of
+ * userCard are Name, Avatar, And userId -> to UserAccount 
+ * that we need to fetch all the user info by Id
+ */
+
 const UserCard = React.memo((props) => {
+  console.log("Re-render from userCard");
   return (
     <div>
       
