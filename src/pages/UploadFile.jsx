56616a18b6b56a6b1c56779a0a33211be4159545
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-08-25 12:37:26 +0700

    #code

--- src/pages/UploadFile.jsx
+++ src/pages/UploadFile.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
 
+/**
+ * @ Post Image URL
+ * @ avatar user URL
+ * */
+
 const UploadFile = () => {
-  /*@ Post Image URL @*/
   const postImgURL = "https://scontent.fdad4-1.fna.fbcdn.net/v/t1.30497-1/143086968_2856368904622192_1959732218791162458_n.png?_nc_cat=1&ccb=1-7&_nc_sid=7206a8&_nc_ohc=5u62LPpCL0kAX8HfD6c&_nc_ht=scontent.fdad4-1.fna&oh=00_AfB7Ra6szL7-CfX_YUIiLJeeMKyq_sq0yQHjFUWmqurZFQ&oe=650EFC78"
   const [image, setImage] = useState(postImgURL);
 
