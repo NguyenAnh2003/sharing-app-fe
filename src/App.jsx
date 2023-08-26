15fdf88c8438894c3c2de63c6860dbd531af7246
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-08-26 16:33:33 +0700

    #code

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import React from 'react';
 
 // apply move to top button on this?
 function App() {
+  useEffect(() => {
+    const fetchAPI = async () => {
+      /**
+       * token from cookie stored in header
+       */
+      const data = await getCurrentUser();
+      console.log(data);
+    };
+    fetchAPI();
+  }, []);
+  
   return <AppRoutes/>;
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { getCurrentUser } from '../libs/apis/authAPI';
 
 /* Replace for search page */
 
 const HomePage = () => {
+
   return (
     <div>
       <p>Home page the main</p>
