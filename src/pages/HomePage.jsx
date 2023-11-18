d84db8e79d7739390eaa5f03e5380efa21029270
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-11-18 21:44:24 +0700

    #code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { useEffect } from 'react';
 import { getCurrentUser } from '../libs/apis/auth.api';
 import { getToken } from '../libs';
+import { useNavigate } from 'react-router-dom';
 
 /* Replace for search page */
 
 const HomePage = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await getCurrentUser();
-      console.log('current user', data);
+      try {
+        const { data } = await getCurrentUser();
+        console.log('current user', data);
+      } catch (error) {
+        if(error.status === 403) navigate('/signin')
+      }
     };
     fetchData();
   }, []);
