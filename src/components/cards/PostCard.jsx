caf61ee70e940c8cfbdd9c8da67af94ad33fa452
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-01-17 23:23:42 +0700

    #code

--- src/components/cards/PostCard.jsx
+++ src/components/cards/PostCard.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { getCategoryById, getDataByPostId, getUserById } from '../../libs';
+import { useSelector } from 'react-redux';
 
 const PostCard = React.memo(({ postId }) => {
-  console.log(postId);
+  const currentUser = useSelector((state) => state.currentUser.userId);
   /**
    * Post data ()
    * @param userId
@@ -28,13 +29,12 @@ const PostCard = React.memo(({ postId }) => {
        * @param commentData
        * -> using Promise.all()
        */
-      
+
       const { data, status } = await getDataByPostId(postId);
       if (status === 200 && data) {
         setPostData(data);
         const { data: userData, status: userStatus } = await getUserById(data.userId);
         if (userStatus === 200 && data) {
-          console.log(userData);
           setUserData(userData);
         }
 
@@ -42,48 +42,48 @@ const PostCard = React.memo(({ postId }) => {
           data.categoryId
         );
         if (categoryStatus === 200 && categoryData) {
-          console.log('category', categoryData);
           setCategoryData(categoryData);
         }
       }
-      console.log(data);
     };
 
     /** call */
     fetchDataa();
 
-    /** validate user info with custom edit */
-
     /** remove postData */
     return () => {
-      setPostData({});
-      setUserData({});
-      setCategoryData({});
+      setPostData({}); //
+      setUserData({}); //
+      setCategoryData({}); //
     };
-  }, [postId]);
+  }, [postId, currentUser]);
 
-  console.log('Re-render from postCard?');
   return (
     postData && (
       <div>
         {/** post content */}
-        <div class="border border-gray-400 lg:border-gray-400 bg-white p-4 flex flex-col justify-between leading-normal">
-          <div class="mb-8">
+        <div className="border border-gray-400 lg:border-gray-400 bg-white p-4 flex flex-col justify-between leading-normal">
+          <div className="mb-8">
             <div className="flex flex-row items-center gap-5 mb-3">
-              <div class="text-gray-900 font-bold text-xl">{postData.title}</div>
+              <div className="text-gray-900 font-bold text-xl">{postData.title}</div>
               {/** category */}
-              <p class="text-primary font-semibold">{categoryData.category}</p>
+              <p className="text-primary font-semibold">{categoryData.category}</p>
             </div>
-            <p class="text-gray-700 text-base">{postData.description}</p>
+            <p className="text-gray-700 text-base">{postData.description}</p>
             {/** post image */}
-            <img className='w-full mt-3' src={postData.imageURL} alt={postData.id} />
+            <img className="w-full mt-3" src={postData.imageURL} alt={postData.id} />
           </div>
-          <div class="flex items-center">
-            <img class="w-10 h-10 rounded-full mr-4" src={userData.imageURL} alt={userData.name} />
-            <div class="text-sm">
-              <p class="text-primary font-semibold">{userData.name}</p>
+          <div className="flex items-center">
+            <img
+              className="w-10 h-10 rounded-full mr-4"
+              src={userData.imageURL}
+              alt={userData.name}
+            />
+            <div className="text-sm">
+              <p className="text-primary font-semibold">{userData.name}</p>
             </div>
           </div>
+          {userData && userData.id === currentUser.userId ? <p>validate user</p> : <>nope</>}
         </div>
       </div>
     )
