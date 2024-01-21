8a0d9251ce955cd3d066a00a661d355a27d4fad6
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-01-21 11:04:56 +0700

    #code

--- src/libs/apis/category.api.js
+++ src/libs/apis/category.api.js
@@ -2,7 +2,7 @@ import { getHTTP } from '../../configs/api.config';
 
 export const getCategories = () => {
   /** get all categories */
-  const res = getHTTP();
+  const res = getHTTP('category');
   return res;
 };
 
--- src/pages/CreatePostPage.jsx
+++ src/pages/CreatePostPage.jsx
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import UploadFile from '../components/UploadFile';
 import Input from '../components/Input';
 import { useSelector } from 'react-redux';
+import toast from 'react-hot-toast';
+import { createPost, getCategories } from '../libs';
 
 const CreatePostPage = () => {
   const currentUser = useSelector((state) => state.currentUser.userId);
+  const titleRef = useRef();
+  const desRef = useRef();
+  const [categoryId, setCategoryId] = useState();
+  const [options, setOptions] = useState([]); // options of categories
+  const [imageUrl, setUrl] = useState();
+
   /**
    * @param title
    * @param description
@@ -12,7 +20,44 @@ const CreatePostPage = () => {
    * @param categoryId -> fetch category
    * @param imageUrl
    */
-  const [imageUrl, setUrl] = useState();
+
+  /** fetching categories */
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const { data, status } = await getCategories();
+      if (status === 200) {
+        console.log('categories', data);
+        setOptions(data);
+      }
+    };
+    fetchCategories();
+  }, []);
+
+  const createHandler = useCallback(async () => {
+    try {
+      if (
+        titleRef.current.value === '' ||
+        desRef.current.value === '' ||
+        categoryId === '' ||
+        imageUrl === ''
+      )
+        toast.error('Fill info first');
+      else {
+        const { data, status } = await createPost(
+          currentUser.userId,
+          categoryId,
+          titleRef.current.value,
+          desRef.current.value,
+          imageUrl
+        );
+        if (status === 200) {
+          toast.success('Create post successfully');
+        }
+      }
+    } catch (error) {
+      toast.error(error.data);
+    }
+  }, [currentUser]);
 
   return (
     <div className="container-2xl mx-10">
@@ -20,11 +65,12 @@ const CreatePostPage = () => {
 
       <div className="grid grid-cols-4 gap-12">
         {/** form for create post */}
-        <div className="col-span-3">
-          <form className="col-span-2 flex flex-col">
+        <div className="col-span-3 pr-40">
+          <form className="col-span-2">
             <div className="flex flex-col gap-4 mb-4">
               <Input />
             </div>
+            {/** categories */}
           </form>
         </div>
         {/** upload file form */}
