19394c80a0adac0448705b1bb6237e62da8d06e1
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-01-21 10:19:26 +0700

    #code

--- src/pages/UploadFile.jsx
+++ src/pages/UploadFile.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useMemo, useRef } from 'react';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import toast from 'react-hot-toast';
@@ -12,35 +12,37 @@ import { uploadFile } from '../libs';
 
 const UploadFile = ({ setUrl }) => {
   /** set image url */
-  const [image, setImage] = useState(null);
+  const [file, setFile] = useState(null);
   const currentUser = useSelector((state) => state.currentUser.userId);
+  const [img, setImg] = useState();
 
   const fileChangeHandler = (e) => {
-    setImage(e.target.files[0]);
+    setFile(e.target.files[0]);
   };
 
   const uploadHandler = useCallback(async () => {
     try {
       const formData = new FormData();
-      formData.append('file', image);
+      formData.append('file', file);
 
       const { data, status } = await uploadFile(formData); // form data
       if (status === 200) {
         // setImage(data);
         console.log('image', data);
+        setImg(data);
         toast.success('Uploaded successfully');
       }
     } catch (error) {
       console.error(error);
       toast.error(error.data);
     }
-  }, [currentUser, image]);
+  }, [currentUser, file]);
 
   return (
     <div>
       <p>Module test for uploading</p>
       <input type="file" onChange={fileChangeHandler} />
-      <img src={image}></img>
+      <img src={img}></img>
       <button
         onClick={uploadHandler}
         className="font-semibold py-2 px-4 w-full rounded bg-btn text-white"
